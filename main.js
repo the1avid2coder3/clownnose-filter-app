adf5891d8e22585f830ef4e8cec9f3d7ca48c84b
the1avid2coder3 <107455484+the1avid2coder3@users.noreply.github.com>
2022-11-21 15:44:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+clown_nose = loadImage("https://i.postimg.cc/cJLrnxbV/1201408-small.jpg");
+}
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+
+function draw() {
+image(video, 0, 0, 300, 300);
+//fill(255,0,0);
+//stroke(255,0,0);
+//circle(noseX, noseY, 40);
+image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png')
+}
+
+
+function modelLoaded() {
+console.log('PoseNet Is Initialised');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x -10;
+        noseY = results[0].pose.nose.y -10;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+    
+}
